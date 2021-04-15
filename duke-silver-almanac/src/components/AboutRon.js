5b6c55ffa116bebc64d86a04b83d91c12a6a198f
rhammer35 <rhammer35@icloud.com>
2021-04-15 00:32:06 -0400

    added bio section

--- duke-silver-almanac/src/components/AboutRon.js
+++ duke-silver-almanac/src/components/AboutRon.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Typography } from '@material-ui/core'
+import { Container, Typography } from '@material-ui/core'
 
 export default function AboutRon() {
   return (
-    <Typography variant='h2'>About Ron</Typography>
+    <Container maxWidth='lg'>
+      <Typography variant='h2' gutterBottom={true}>About Ron</Typography>
+      <Typography variant='body1'>
+        Ronald Ulysses "Ron" Swanson is a fictional character portrayed by Nick Offerman in the NBC comedy-drama Parks and Recreation. He is the former husband of two women by the name of Tammy Swanson and is the current husband of Diane Swanson.
+      </Typography>
+      <br/>
+      <Typography variant='body1'>
+        He was Leslie Knope's deadpan superior as the head of the Pawnee Parks and Recreation Department until she left the department to become a city councilor, and then again when she rejoined the department after she was recalled. In late 2014, he quit the Parks department and started Very Good Building and Development Co. 
+      </Typography>
+      <br/>
+      <Typography variant='body1'>
+        (Source: Parks and Recreation Fandom Wikia)
+      </Typography>
+    </Container>
   )
 }
